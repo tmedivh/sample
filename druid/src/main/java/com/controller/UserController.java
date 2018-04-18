35fac485fab3e5e14c2e3f17ee8b3a25439caaba
miao <formedivh@163.com>
2018-04-18 19:21:56 +0800

    阿里数据源配置

--- druid/src/main/java/com/controller/UserController.java
+++ druid/src/main/java/com/controller/UserController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class UserController {
+
     @Autowired
     private UserInfoMapper userInfoMapper;
 
