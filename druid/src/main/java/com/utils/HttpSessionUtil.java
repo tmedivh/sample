d6687d9ffc7be5e6e60c7a8eed7d57d0dac7cdb3
miao <formedivh@163.com>
2018-04-22 21:26:37 +0800

    打包配置修改

--- druid/src/main/java/com/utils/HttpSessionUtil.java
+++ druid/src/main/java/com/utils/HttpSessionUtil.java
@@ -3,6 +3,7 @@ package com.utils;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -29,5 +30,14 @@ public class HttpSessionUtil {
      */
     public static HttpSession getHttpSession() {
         return servletRequestAttributes.getRequest().getSession();
+    }
+
+    /**
+     * 获取当前请求的session
+     *
+     * @return
+     */
+    public static HttpServletRequest getHttpRequest() {
+        return servletRequestAttributes.getRequest();
     }
 }
