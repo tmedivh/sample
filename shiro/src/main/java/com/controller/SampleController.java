d591cf26198784119da5f944a708ea8368cec58e
miaoyusong <miaoyusong@wzdai.com>
2018-07-06 16:33:53 +0800

    新增shiro

--- shiro/src/main/java/com/controller/SampleController.java
+++ shiro/src/main/java/com/controller/SampleController.java
@@ -1,14 +1,5 @@
 package com.controller;
 
-import com.annotation.Log;
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
-import com.mapper.UserInfoMapper;
-import com.model.UserInfo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,25 +12,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class SampleController {
-
-    @Autowired
-    private UserInfoMapper userInfoMapper;
-
-    @GetMapping
-    @Log
-    public Object test() {
-        PageHelper.startPage(1, 15);
-        throw new RuntimeException("12312");
-        //return new PageInfo<>(userInfoMapper.selectAll());
-    }
-
-    @PutMapping("/update")
-    @Transactional
-    @Log
-    public Object update() {
-        UserInfo userInfo = new UserInfo();
-        userInfo.setId(1);
-        userInfo.setName("miao");
-        return userInfoMapper.updateByPrimaryKeySelective(userInfo);
-    }
 }
